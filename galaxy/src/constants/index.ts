95432a8835493c01acb0e8d1745bccfad4371a76
Akshay ghar <183997642+AKSHAYGHAR@users.noreply.github.com>
2026-02-01 10:45:36 +1400

    Delete galaxy directory

--- galaxy/src/constants/index.ts
+++
@@ -1,8 +0,0 @@
-export const APP_NAME = "GALAXY";
-export const APP_DESCRIPTION = "A minimal wallpaper library";
-
-export const ROUTES = {
-    HOME: "/",
-    DESKTOP: "/desktop",
-    MOBILE: "/mobile",
-};
