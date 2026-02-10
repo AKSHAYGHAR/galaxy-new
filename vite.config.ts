3b400835a18e8b74e10483e055a6737015feaf23
AKSHAYGHAR <183997642+AKSHAYGHAR@users.noreply.github.com>
2026-02-10 17:04:41 +1400

    Update favicon and cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => ({
     mode === "development" && componentTagger(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt'],
+      includeAssets: ['favicon.png', 'robots.txt'],
       manifest: {
         name: 'Standard Wallpapers',
         short_name: 'Standard',
